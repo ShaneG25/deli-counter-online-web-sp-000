c5b24f4b8b97d0b37cc4340d2d9665cf2cc745a8
Shane Geary <stg4462@comcast.net>
2020-01-24 19:37:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
-Katz_deli = [] 
+katz_deli = [] 
 
-def line(Katz_deli)
-  if (Katz_deli.length == 0)
+def line(katz_deli)
+  if (katz_deli.length == 0)
     puts "The line is currently empty."
     return "The line is currently empty."
   else 
     callOut = "The line is currently:"
-    Katz_deli.each_with_index do |person, index|
+    katz_deli.each_with_index do |person, index|
       place = index + 1 
       callOut += " #{place}. #{person}"
     end 
