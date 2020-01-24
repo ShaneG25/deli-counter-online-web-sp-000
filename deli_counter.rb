2e90b9d584ecfa4f998a4f57b62b4777e3df6edd
Shane Geary <stg4462@comcast.net>
2020-01-24 19:44:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,3 +21,12 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name} you are number #{place} in line."
 end 
 
+def now_serving(katz_deli)
+  if(katz_deli.length <= 0)
+    puts "There is nobody waiting to be served!"
+  elsif(katz_deli.length >= 1) 
+    person = katz_deli[0] 
+    puts "Currently serving #{person}."
+    katz_deli.shift
+  end 
+end 
